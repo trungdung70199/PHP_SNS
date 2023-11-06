6850bab0f11450cd96d67e8e7e18476f44218e44
trungdung70199 <trungdung70199@gmail.com>
2023-11-06 14:18:01 +0900

    update

--- app/views/tweet/index.view.php
+++ app/views/tweet/index.view.php
@@ -19,17 +19,7 @@
                             <?= $tweet['message'] ?>
                         </div>
                         <div class="tweet-nav mb-3">
-                            <form action="like.php" method="post">
-                                <?php if (in_array($tweet['id'], $user_likes)): ?>
-                                <button class="btn btn-sm"><img src="../images/svg/heart_active.svg"></button>
-                                <span class="like-count"><?= @$like_counts[$tweet['id']] ?></span>
-                                <?php else: ?>
-                                <button class="btn btn-sm"><img src="../images/svg/heart.svg"></button>
-                                <span class="like-count"><?= @$like_counts[$tweet['id']] ?></span>
-                                <?php endif ?>
-                                <input type="hidden" name="user_id" value="<?= $user['id'] ?>">
-                                <input type="hidden" name="tweet_id" value="<?= $tweet['id'] ?>">
-                            </form>
+                            <?php include(VIEW_DIR . 'components/like_form.php'); ?>
                         </div>
                     </div>
 
